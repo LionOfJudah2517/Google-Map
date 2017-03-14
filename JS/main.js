d46af1a5607abba23bc1533a61d55424da1d0bcb
Scott O'Neil <ScottO@Scotts-MBP.fios-router.home>
2017-03-14 12:04:03 -0400

    created a bostonLocations variable, currently trouble shooting irrecognizable variable

--- JS/main.js
+++ JS/main.js
@@ -22,7 +22,7 @@ function initMap() {
   }); */
 
   //Create an array of Objects to be used, minimum of 5.
-var locations = [
+var bostonLocations = [
   {title: 'Emmanuel College', location: {lat:42.341625, lng:-71.102528}},
   {title: 'House of Blues', location: {lat:42.347328, lng:-71.095471}},
   {title: 'Fenway Park', location: {lat:42.346497, lng:-71.097184}},
@@ -33,9 +33,9 @@ var locations = [
 var largeInfoWindow = new google.maps.InfoWindow();
 var bounds = new google.maps.LatLngBounds();
 
-for (var i = 0; i < locations.length; i++){
-  var position = locations[i].location;
-  var title = locations[i].location;
+for (var i = 0; i < bostonLocations.length; i++){
+  var position = bostonLocations[i].location;
+  var title = bostonLocations[i].title;
   var marker = new google.maps.Marker({
     map: map,
     position: position,
@@ -49,7 +49,7 @@ for (var i = 0; i < locations.length; i++){
     populateInfoWindow(this, largeInfoWindow);
 
   });
-}
+};
   map.fitBounds(bounds);
 
 function populateInfoWindow(marker, infowindow){
@@ -62,16 +62,21 @@ function populateInfoWindow(marker, infowindow){
       infowindow.setMarker(null);
     });
   }
-}
-
+};
 
 
 //Sidebar navigation JS
 }
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-}
+};
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+};
+function sideBarTitles(){
+for(var i = 0; i < bostonLocations.length; i++){
+  $("#mySidenav").append("<li>" + bostonLocations.title[i] + "</li>")
 }
+
+};
