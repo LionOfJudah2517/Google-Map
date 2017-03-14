abee0020c020b9414c3e1ac55d3ce1191ac5c1e8
Scott O'Neil <ScottO@Scotts-MBP.fios-router.home>
2017-03-14 12:34:48 -0400

    Debugging solved, put variable into a global position

--- JS/main.js
+++ JS/main.js
@@ -4,7 +4,14 @@ var map;
 
 // Create a new blank array for all the listing markers.
 var markers = [];
-
+//Create an array of Objects to be used, minimum of 5.
+var bostonLocations = [
+  {title: 'Emmanuel College', location: {lat:42.341625, lng:-71.102528}},
+  {title: 'House of Blues', location: {lat:42.347328, lng:-71.095471}},
+  {title: 'Fenway Park', location: {lat:42.346497, lng:-71.097184}},
+  {title: 'Boston University', location: {lat:42.350164, lng:-71.104722}},
+  {title: 'Northeastern University', location: {lat:42.340068, lng:-71.088966}}
+];
 //function to initialize the map
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -13,22 +20,7 @@ function initMap() {
     //set zoom strength 1-21
     zoom: 15
   });
-  //Emmanuel College in same neighborhood as Fenway park.
-  /*var emmanuel = {lat:42.341625, lng:-71.102528};
-  var marker = new google.maps.Marker({
-    position: emmanuel,
-    map: map,
-    title: 'Emmanuel College!'
-  }); */
 
-  //Create an array of Objects to be used, minimum of 5.
-var bostonLocations = [
-  {title: 'Emmanuel College', location: {lat:42.341625, lng:-71.102528}},
-  {title: 'House of Blues', location: {lat:42.347328, lng:-71.095471}},
-  {title: 'Fenway Park', location: {lat:42.346497, lng:-71.097184}},
-  {title: 'Boston University', location: {lat:42.350164, lng:-71.104722}},
-  {title: 'Northeastern University', location: {lat:42.340068, lng:-71.088966}}
-];
 
 var largeInfoWindow = new google.maps.InfoWindow();
 var bounds = new google.maps.LatLngBounds();
@@ -62,21 +54,21 @@ function populateInfoWindow(marker, infowindow){
       infowindow.setMarker(null);
     });
   }
+}
 };
 
+var sideBarTitles = function(){
+for(var i = 0; i < bostonLocations.length; i++){
+  $("#mySidenav").append("<li>" + bostonLocations[i].title + "</li>")
+}
+}();
 
 //Sidebar navigation JS
-}
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
 };
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-};
-function sideBarTitles(){
-for(var i = 0; i < bostonLocations.length; i++){
-  $("#mySidenav").append("<li>" + bostonLocations.title[i] + "</li>")
-}
-
 };
