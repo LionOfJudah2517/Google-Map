10817c9683e7046da5f241307a028655cab9b877
Scott O'Neil <ScottO@Scotts-MacBook-Pro.local>
2017-03-28 10:39:52 -0400

    Added comments, working on JSON request

--- JS/main.js
+++ JS/main.js
@@ -3,8 +3,13 @@
 //Create variables
 var map;
 var markers;
+var twitter;
 
-//Create an array of Objects to be used, minimum of 5.
+
+/*
+*********** MODEL TO STORE INFORMATION **********
+
+*/
 var bostonLocations = [
   {name: 'Emmanuel College', lat:42.341625, lng:-71.102528},
   {name: 'House of Blues', lat:42.347328, lng:-71.095471},
@@ -13,6 +18,10 @@ var bostonLocations = [
   {name: 'Northeastern University', lat:42.340068, lng:-71.088966}
 ];
 
+/*
+************ OBJECT GENERATOR ***********
+
+*/
 
 var LocationGenerator = function(map, locData){
 var self = this;
@@ -27,35 +36,40 @@ this.lng = locData.lng;
 	//this.city = city;
 this.visible = ko.observable(true);
 
-
+//FOURSQUARE API CLIENT INFORMATION
   var clientID= 'MNULAPVKHSXV4G3UB3GZKJ2Z4F55JUOAXW0N5TR4SLJY0TSH';
   var clientSecret = 'PGFSG12KQNWEG3G21J2BFJRVGDR5OMFXZTB2G2VPO5I0WEGD';
 
 	var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll='+ locData.lat + ',' + locData.lng + '&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20161016' + '&query=' + this.name;
 
+
+
+//****** GET JSON REQUEST FOR FOURSQUARE SOCIAL MEDIA
 this.twitter;
+  $.getJSON(foursquareURL).done(function(cat){
+       self.twitter = cat.response.venues[0].contact.twitter;
 
-$.getJSON(foursquareURL,function(cat){
-      console.log(cat.response.venues[0].contact.twitter);
+    });
 
-});
+console.log(this.twitter);
 
-
-
-//This info goes into the infowindow box
+//********CONTENT THAT IS GOING TO FILL INFOWINDOW BOX.
 this.contentString = `<h2>${this.name}</h3><h4>${this.lat}, ${this.lng}</h4><p>Twitter:${this.twitter}</p>`;
 
-
+//********INFOWINDOWS FOR EACH MARKER
 this.infowindow = new google.maps.InfoWindow({
   content: this.contentString
 });
 
+//******** CREATE MARKERS WITH ESSENTIAL DATA FOR DISPLAY
 this.marker = new google.maps.Marker({
           map: self.map,
           position: new google.maps.LatLng(locData.lat, locData.lng),
           name: locData.name
 });
 
+
+//******** KNOCKOUT FUNCTIONALITY FOR MARKERS
   this.showMarker = ko.computed(function() {
 		if(this.visible() === true) {
 			this.marker.setMap(map);
@@ -83,11 +97,14 @@ var MyAppView = function(){
 
   this.bostonSearch = ko.observable('');
 
-  this.map = map = new google.maps.Map(document.getElementById('map'), {
+  this.map = new google.maps.Map(document.getElementById('map'), {
     //Fenway Park Boston MA 42.3466764,-71.0994065,
     center: {lat: 42.3466764, lng: -71.0994065},
     //set zoom strength 1-21
     zoom: 13
+
+
+
   });
 
   bostonLocations.forEach(function(bostonLocation){
@@ -96,32 +113,43 @@ var MyAppView = function(){
 
 
     this.bostonFilter = ko.computed(function() {
-
       var filtered = self.bostonSearch().toString().toLowerCase();
-      if (!filtered) {
-			  self.bosLocationList().forEach(function(bostonLoc){
-				bostonLoc.visible(true);
-			});
-
-			return self.bosLocationList();
+        if (!filtered) {
+			       self.bosLocationList().forEach(function(bostonLoc){
+				           bostonLoc.visible(true);
+			              });
+			                 return self.bosLocationList();
 		} else {
-			return ko.utils.arrayFilter(self.bosLocationList(), function(bostonLoc) {
-				var string = bostonLoc.location.name.toLowerCase();
-				var result = (string.search(filtered) >= 0);
-				bostonLoc.visible(result);
-				return result;
+			  return ko.utils.arrayFilter(self.bosLocationList(), function(bostonLoc) {
+				   var string = bostonLoc.location.name.toLowerCase();
+				   var result = (string.search(filtered) >= 0);
+				       bostonLoc.visible(result);
+				            return result;
 			});
 		}
 	}, self);
 
 };
 
+
+/*
+
+**********INITIALIZE THE GOOGLE MAP*************
+
+*/
 var myMap;
 var initMap = function(){
   myMap = MyAppView;
 ko.applyBindings(myMap);
 };
 
+
+
+/*
+
+***********SIDE-BAR FUNCTIONALITY***************
+
+*/
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
 };
